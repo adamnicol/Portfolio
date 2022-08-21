0563ef9b3b6eda9af2bcd30b581e558788d85720
Adam Nicol <adam.nicol@protonmail.com>
2022-08-21 16:13:58 +0000

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./css/bootstrap.min.css";
+import "./css/style.css";
+
+function App() {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
