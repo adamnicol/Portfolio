bf93a24a802ff855caac05239fe1fabb80414e99
Adam Nicol <adam.nicol@protonmail.com>
2022-09-25 16:59:42 +0100

    Added error handling to the login

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -2,28 +2,52 @@ import { FormEvent, useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useModal } from "./providers/ModalProvider";
 import { useAuth } from "./providers/AuthProvider";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import Status from "./../utils/statusCodes";
 
 function Login() {
   const [email, setEmail] = useState<string>();
   const [password, setPassword] = useState<string>();
-  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [waiting, setWaiting] = useState<boolean>(false);
 
   const modal = useModal();
   const auth = useAuth();
 
   function login(e: FormEvent) {
     e.preventDefault();
-    axios.post("/users/login", { email, password }).then((response) => {
-      auth.setCurrentUser(response.data);
-      modal.close();
-    });
+    setError(null);
+    setWaiting(true);
+
+    axios
+      .post("/users/login", { email, password })
+      .then((response) => {
+        auth.setCurrentUser(response.data);
+        modal.close();
+      })
+      .catch((error: AxiosError) => {
+        setWaiting(false);
+        handleError(error);
+      });
+  }
+
+  function handleError(error: AxiosError) {
+    if (error.response?.status === Status.Unauthorized) {
+      setError("Invalid username or password");
+    } else if (error.response?.status === Status.TooManyRequests) {
+      setError("Too many login attempts");
+    } else if (error.response?.status) {
+      setError("Server returned error " + error.response.status);
+    } else {
+      setError("Server is not responding");
+    }
   }
 
   return (
     <div className="login-form">
       <h2>Login</h2>
-      <Form noValidate validated={validated} onSubmit={login}>
+
+      <Form onSubmit={login}>
         <Form.Group controlId="email" className="mb-3">
           <Form.Control
             type="email"
@@ -51,9 +75,12 @@ function Login() {
         </Form.Group>
 
         <Button type="submit" value="Login" className="w-100">
+          {waiting && <span className="spinner-border spinner-border-sm" />}{" "}
           Login
         </Button>
       </Form>
+
+      {error && <div className="alert alert-danger mt-3">{error}</div>}
 
       <p className="mt-4">
         Don't have an account? <a className="link-primary">Register</a>
---
+++ client/src/utils/statusCodes.ts
@@ -0,0 +1,14 @@
+enum Status {
+  OK = 200,
+  Created = 201,
+  Accepted = 202,
+  BadRequest = 400,
+  Unauthorized = 401,
+  Forbidden = 403,
+  NotFound = 404,
+  Conflict = 409,
+  TooManyRequests = 429,
+  Error = 500,
+}
+
+export default Status;
