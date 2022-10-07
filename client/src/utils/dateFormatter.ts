e9655bcb9b1ad08235607455f9c47aa9102f0ffd
Adam Nicol <adam.nicol@protonmail.com>
2022-10-07 20:32:33 +0100

    Changed to relative time

--- client/src/utils/dateFormatter.ts
+++ client/src/utils/dateFormatter.ts
@@ -1,18 +1,8 @@
 import dayjs from "dayjs";
-import advancedFormat from "dayjs/plugin/advancedFormat";
-import isToday from "dayjs/plugin/isToday";
-import isYesterday from "dayjs/plugin/isYesterday";
 import relativeTime from "dayjs/plugin/relativeTime";
 
+dayjs.extend(relativeTime);
+
 export function formatDate(date: Date): string {
-  dayjs.extend(advancedFormat);
-  dayjs.extend(isToday);
-  dayjs.extend(isYesterday);
-  dayjs.extend(relativeTime);
-
-  const d = dayjs(date);
-
-  if (d.isToday()) return d.fromNow();
-  else if (d.isYesterday()) return "Yesterday";
-  else return d.format("MMM Do, YYYY");
+  return dayjs(date).fromNow();
 }
