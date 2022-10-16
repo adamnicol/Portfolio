b77c6ab5a54709bb0a54a4614739d5d4f9da5ff1
Adam Nicol <adam.nicol@protonmail.com>
2022-10-16 20:33:26 +0100

    Moved api url to .env

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import config from "../utils/config";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: config.API_URL,
   withCredentials: true,
 });
 
