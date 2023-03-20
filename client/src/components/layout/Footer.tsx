c2a9e019af500f8f461ab9abced9456713ac2002
Adam Nicol <adam.nicol@protonmail.com>
2023-03-20 17:59:03 +0000

    Change footer styles

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { menuLinks } from "@data/navigation";
 import { socials } from "@data/socials";
 
@@ -9,24 +9,29 @@ function Footer() {
       <div className="d-flex mb-2">
         <ul className="list-unstyled">
           {menuLinks.map((link, index) => (
-            <li key={index} className="mt-1">
-              <FontAwesomeIcon icon={link.icon} className="me-3" fixedWidth />
-              <Link to={link.path} className="text-secondary">
+            <li key={index} className="mt-2">
+              <FontAwesomeIcon icon={link.icon} fixedWidth />
+              <NavLink
+                to={link.path}
+                className={({ isActive }) =>
+                  "text-secondary " + (isActive ? "nav-link-active" : "nav-link")
+                }
+              >
                 {link.text}
-              </Link>
+              </NavLink>
             </li>
           ))}
         </ul>
         <ul className="list-unstyled ms-5">
           {socials.map((link, index) => (
-            <li key={index} className="mt-1">
-              <FontAwesomeIcon icon={link.icon} className="me-3" fixedWidth />
+            <li key={index} className="mt-2">
+              <FontAwesomeIcon icon={link.icon} fixedWidth />
               <a
                 href={link.url}
                 title={link.name}
                 target="_blank"
                 rel="noreferrer"
-                className="text-secondary"
+                className="nav-link text-secondary"
               >
                 {link.name}
               </a>
