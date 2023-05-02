450ba86094d13651c9a0311cb47eca9cad4b04ef
Adam Nicol <adam.nicol@protonmail.com>
2023-05-02 21:05:43 +0100

    Extend button attributes

--- client/src/components/button/SpinButton.tsx
+++ client/src/components/button/SpinButton.tsx
@@ -1,16 +1,16 @@
 import { Button, Spinner } from "react-bootstrap";
+import { ButtonHTMLAttributes } from "react";
 
-interface ISpinButtonProps {
+interface SpinButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
   loading: boolean;
-  className?: string;
 }
 
-function SpinButton(props: ISpinButtonProps) {
-  const { text, loading, className } = props;
+export function SpinButton(props: SpinButtonProps) {
+  const { text, loading, ...attributes } = props;
 
   return (
-    <Button type="submit" className={className} disabled={loading}>
+    <Button type="submit" disabled={loading} {...attributes}>
       {loading && <Spinner as="span" animation="border" size="sm" />} {text}
     </Button>
   );
